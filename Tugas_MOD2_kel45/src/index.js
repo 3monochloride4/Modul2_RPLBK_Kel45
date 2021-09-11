f6b7bed5e691ff84690fdc14768b1fb6adeb706d
KholidTheBetweener <50976657+KholidTheBetweener@users.noreply.github.com>
2021-09-11 17:03:38 +0700

    update index.js
    adding quotes

--- Tugas_MOD2_kel45/src/index.js
+++ Tugas_MOD2_kel45/src/index.js
@@ -5,15 +5,21 @@ import App from "./App";
 
 import CardList from './components/CardList';
 import Potret from './assets/images/8434_2.jpg'; 
+import Quotes from "./components/Quotes";
 
+const quotes =
+"People Die When They're Killed";
+const books =
+"Fate/Unlimited Blade Works";
 ReactDOM.render(
   <React.StrictMode>
     <CardList
-      name = 'Zainul Muttaqin' 
-      nim = '21120118130073' 
-      kelompok = '0' 
+      name = 'Zainul Muttaqin and Kholid Syaifullah' 
+      nim = '21120118130073 and 21120118120033' 
+      kelompok = '45' 
       isNameBold
       image = {Potret}/>
+<Quotes author="Shirou" quotes={quotes} books={books}/>
   </React.StrictMode>,
   document.getElementById("root")
 );
