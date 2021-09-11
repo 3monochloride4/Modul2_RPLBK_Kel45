db64ff5c10e15221734b361d04a7a3fc56c51155
KholidTheBetweener <50976657+KholidTheBetweener@users.noreply.github.com>
2021-09-11 16:54:10 +0700

    Update App.js
    change all

--- Tugas_MOD2_kel45/src/App.js
+++ Tugas_MOD2_kel45/src/App.js
@@ -2,14 +2,14 @@ function App() {
   return (
     <div className="C">
       <div
-        className="flex items-center justify-center bg-gray-50"
-        style={{ height: "100vh" }}
+        className="flex items-center justify-center bg-indigo-50"
+        style={{ height: "30vh" }}
       >
-        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl text-center">
-          <span className="block">Selamat Datang di Praktikum RPLBK</span>
-          <span className="block text-indigo-600">Modul 2</span>
-          <p className="text-center text-gray-300 font-normal">
-            "Ini baru setup dan konfigurasi projek yaa :)"
+        <h2 className="text-3xl font-serif font-extrabold tracking-tight text-gray-900 sm:text-4xl text-center">
+          <span className="block text-indigo-700">Tugas Modul 2</span>
+          <span className="block text-indigo-600">Kelompok 45</span>
+          <p className="text-center text-indigo-600 font-normal">
+            "Komponen Dan Props React"
           </p>
         </h2>
       </div>
