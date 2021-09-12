1edd0c802a663cb43af1300465bc501b33e509db
3monochloride4 <49035134+3monochloride4@users.noreply.github.com>
2021-09-12 12:06:50 +0700

    update Index.js
    I add several new lines and update some previous lines. now, index.js has supported arrays of object input as props

--- Tugas_MOD2_kel45/src/components/CardList/index.js
+++ Tugas_MOD2_kel45/src/components/CardList/index.js
@@ -1,40 +1,54 @@
 import React from "react";
 
 export default function CardList(props) {
-  const { name, nim, kelompok, isNameBold, image } = props;
+  const { card_data, isNameBold } = props;
 
   return (
-    <div className="font-serif bg-white shadow">
+    <div className="bg-blue-100 shadow">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
-          Praktikan RPLBK 2021
+        <h2 className="font-bold text-2xl tracking-tight text-gray-900">
+          Meet Our Team
         </h2>
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          <div className="group relative shadow">
-            <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-              <img
-                src={image}
-                alt="Gambar"
-                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-              />
-            </div>
-            <div className="mt-4 flex justify-between">
-              <div>
-                <h3
-                  className={`${
-                    isNameBold ? "font-bold " : ""
-                  }  text-sm text-gray-700`}
-                >
-                  <span aria-hidden="true" className="absolute inset-0" />
-                  {name}
-                </h3>
-                <p className="mt-1 text-sm text-gray-500">{nim}</p>
+          {card_data.map(({nama, nim, kelompok, gambar}) => (
+            <div className="rounded-2xl bg-gray-100 group relative shadow-xl">
+              <div className="py-5 px-5 w-80 min-h-80 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-70 lg:aspect-none object-center">
+                <img
+                  src={gambar}
+                  alt="Gambar"
+                  className=" ring-4 ring-blue-300 rounded-full h-60 w-60 object-scaledown"
+                />
               </div>
-              <p className="text-sm font-medium text-gray-900">
-                Kelompok : {kelompok}
-              </p>
+              <div className="mt-5 flex pb-5 pr-5 justify-between">
+                <div>
+                  <h3
+                    className={`${
+                      isNameBold ? "font-bold " : ""
+                    } px-5 text-sm text-gray-700`}
+                  >
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {nama}
+                  </h3>
+                  <p className="px-5 mt-1 text-sm font-medium text-gray-900">{nim}</p>
+                  <p className="px-5 text-sm font-medium text-gray-900">
+                  Kelompok {kelompok}
+                </p>
+                </div>
+                <div>
+                  <img 
+                    src = 'https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-512.png'
+                    className = 'object-scaledown h-6 w-6 mb-3'
+                    alt = 'Instagram logo'
+                  />
+                  <img 
+                    src = 'https://cdn-icons-png.flaticon.com/512/174/174857.png'
+                    className = 'object-scaledown h-6 w-6'
+                    alt = 'Linkedin logo'
+                  />
+                </div>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
